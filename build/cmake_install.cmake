a3fb625b99df146b3479876815cd18140e58c4e7
mvk66 <mvk66@tpu.ru>
2024-11-02 13:55:29 +0700

    Добавлена библиотека SFML

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -37,6 +37,22 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "C:/msys64/mingw64/bin/objdump.exe")
 endif()
 
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "C:/DotaProject/DotaProject.exe")
+  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/DotaProject.exe" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/DotaProject.exe")
+    if(CMAKE_INSTALL_DO_STRIP)
+      execute_process(COMMAND "C:/msys64/mingw64/bin/strip.exe" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/DotaProject.exe")
+    endif()
+  endif()
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("C:/DotaProject/build/_deps/sfml-build/cmake_install.cmake")
+
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
-#include <iostream>
+#include <SFML/Window.hpp>
 
-int main(int, char**){
-    std::cout << "Hello, from DotaProject!\n";
+int main() {
+    // Создаем окно с заданным размером и заголовком
+    sf::Window window(sf::VideoMode(800, 600), "My window");
+
+    // Основной цикл программы, который будет работать, пока окно открыто
+    while (window.isOpen()) {
+        sf::Event event;
+        
+        // Обрабатываем все события окна
+        while (window.pollEvent(event)) {
+            // Если событие закрытия окна, то закрываем его
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+    }
+
+    return 0;
 }
